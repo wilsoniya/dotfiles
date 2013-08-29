a5abf4cab8493446b8c2b2f1008ff0a5fd65162f
Michael Wilson <wilsoniya@gmail.com>
2013-08-29 16:46:58 -0500

    modified installation script to support existing file backup

--- install.sh
+++ install.sh
@@ -4,9 +4,26 @@ SCRIPT_NAME=`basename $0`
 DOTFILES_DIR=~/dotfiles
 FILES=`ls $DOTFILES_DIR`
 
+bu_dir=~/dotfiles_bu
+backup=false
+
 echo ""
-echo "Installing dotfiles. Existing files will not be overwritten..."
+echo "Installing dotfiles. Existing files will backed up in $bu_dir ..."
 echo ""
+
+mkdir $bu_dir 2> /dev/null
+
+for file in $FILES; do
+    f=~/.$file
+    if [ -a $f ]; then
+        backup=true
+        mv $f $bu_dir/$file
+    fi
+done
+
+if ! $backup ; then
+    rmdir $bu_dir
+fi
 
 for file in $FILES; do
     if [[ $file == $SCRIPT_NAME ]]; then
