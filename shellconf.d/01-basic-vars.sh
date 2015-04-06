5afcd44f6e707ea72eb5e4ae953ded0b17e86fb7
Michael Wilson <wilsoniya@gmail.com>
2015-04-06 13:28:33 -0500

    stderr coloring

--- shellconf.d/01-basic-vars.sh
+++ shellconf.d/01-basic-vars.sh
@@ -1,6 +1,12 @@
 export EDITOR=vim
 export OS=$(uname -s)
 
+# ssh-agent connection, if available
 if [[ -e "${XDG_RUNTIME_DIR}/ssh-agent.socket" ]]; then
     export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+fi
+
+# STDERR coloring, if available
+if [ -f "/usr/lib/libstderred.so" ]; then
+    export LD_PRELOAD="/usr/lib/libstderred.so"
 fi
